22a5a3fc3b30bfb638ae110966c675d50559d00a
i.shesholko <i.shesholko@btslogistics.com>
2022-11-13 20:32:36 +0300

    init

---
+++ src/main/java/com/bts/test/TestController.java
@@ -0,0 +1,14 @@
+package com.bts.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public void test() {
+        System.out.println("xxx");
+    }
+
+}
