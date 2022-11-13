2907ae67cee7fbae0fcf9291480cac3e5c1d89ba
i.shesholko <i.shesholko@btslogistics.com>
2022-11-13 20:59:54 +0300

    init

--- src/main/java/com/bts/test/TestController.java
+++ src/main/java/com/bts/test/TestController.java
@@ -1,14 +1,18 @@
 package com.bts.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${some.property}")
+    private String someProperty;
+
     @GetMapping("/test")
     public void test() {
-        System.out.println("xxx");
+        System.out.println(someProperty);
     }
 
 }
